c2af5179e626b0e34649e5ae77a49d7a62fbf05b
AVM.Martin <avmm.martin+github@gmail.com>
2020-04-28 20:02:01 +0700

    Add ScheduleFactory

--- app/src/main/java/id/my/avmmartin/matched/data/db/factory/ScheduleFactory.java
+++ app/src/main/java/id/my/avmmartin/matched/data/db/factory/ScheduleFactory.java
@@ -1,4 +1,45 @@
 package id.my.avmmartin.matched.data.db.factory;
 
+import android.content.Context;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import id.my.avmmartin.matched.data.db.ScheduleManager;
+import id.my.avmmartin.matched.data.db.model.Schedule;
+
 public class ScheduleFactory {
+    public static void generate(Context context) {
+        ScheduleManager db = new ScheduleManager(context);
+
+        if (db.size() == 0) {
+            Calendar startTime = Calendar.getInstance();
+            Calendar endTime = Calendar.getInstance();
+            Random rnd = new Random();
+
+            for (int i = 1; i < 50; i++) {
+                int month = 3 + rnd.nextInt(2); // April and May
+                int day = 1 + rnd.nextInt(30);  // only 1..30
+                int hour = rnd.nextInt(24);
+                int minute = rnd.nextInt(60);
+                int duration = 1 + rnd.nextInt(3);
+
+                startTime.set(2020, month, day, hour, minute, 0);
+
+                endTime.set(2020, month, day, hour, minute, 0);
+                endTime.add(Calendar.HOUR_OF_DAY, duration);
+
+                try {
+                    db.insertSchedule(new Schedule(
+                        "Title " + i,
+                        "location",
+                        startTime.getTime(),
+                        endTime.getTime()
+                    ));
+                } catch (Exception e) {
+                    //
+                }
+            }
+        }
+    }
 }
