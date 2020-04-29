ec259ba4fc0763b115a5d739cb28d776bea01c02
AVM.Martin <avmm.martin+github@gmail.com>
2020-04-30 01:09:34 +0700

    Implement getDeviceId() for preferences

--- app/src/main/java/id/my/avmmartin/matched/utils/CommonUtils.java
+++ app/src/main/java/id/my/avmmartin/matched/utils/CommonUtils.java
@@ -1,7 +1,9 @@
 package id.my.avmmartin.matched.utils;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.provider.Settings;
 
 public class CommonUtils {
     public static ProgressDialog showLoadingDialog(Context context) {
@@ -13,6 +15,11 @@ public class CommonUtils {
         progressDialog.setCanceledOnTouchOutside(false);
 
         return progressDialog;
+    }
+
+    @SuppressLint("HardwareIds")
+    public static String getDeviceId(Context context) {
+        return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
     }
 
     private CommonUtils() {
