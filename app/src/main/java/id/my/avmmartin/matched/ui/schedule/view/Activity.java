0402b20ba355075fca2549be530e1106ac22cf9f
ekeitaro <edwardkeitaro.heru@gmail.com>
2020-04-29 20:52:29 +0700

    UI CustomCalendar

--- app/src/main/java/id/my/avmmartin/matched/ui/schedule/view/Activity.java
+++ app/src/main/java/id/my/avmmartin/matched/ui/schedule/view/Activity.java
@@ -6,26 +6,26 @@ import android.widget.CalendarView;
 
 import id.my.avmmartin.matched.R;
 import id.my.avmmartin.matched.ui.base.BaseActivity;
+import id.my.avmmartin.matched.utils.CustomCalendar;
 
 public class Activity extends BaseActivity<Presenter> implements MVPView {
-    CalendarView calendarView;
+    CustomCalendar cvCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_schedule_view);
         super.onCreate(savedInstanceState);
 
-
     }
 
     @Override
     protected void initComponents() {
-
+        cvCalendar = findViewById(R.id.cvCalendar);
     }
 
     @Override
     protected void loadData() {
-
+        
     }
 
     @Override
