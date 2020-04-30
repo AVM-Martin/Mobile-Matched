cd5ea0f643174ec2079c4aefe4b00b677516acfc
ekeitaro <edwardkeitaro.heru@gmail.com>
2020-04-30 11:16:21 +0700

    UI Add Event 1

---
+++ app/src/main/java/id/my/avmmartin/matched/ui/schedule/add/MVPPresenter.java
@@ -0,0 +1,7 @@
+package id.my.avmmartin.matched.ui.schedule.add;
+
+import id.my.avmmartin.matched.ui.base.BaseMVPPresenter;
+
+interface MVPPresenter extends BaseMVPPresenter<MVPView> {
+
+}
--- app/src/main/java/id/my/avmmartin/matched/ui/schedule/view/Activity.java
+++ app/src/main/java/id/my/avmmartin/matched/ui/schedule/view/Activity.java
@@ -1,15 +1,21 @@
 package id.my.avmmartin.matched.ui.schedule.view;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.widget.CalendarView;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import id.my.avmmartin.matched.R;
 import id.my.avmmartin.matched.ui.base.BaseActivity;
+import id.my.avmmartin.matched.ui.schedule.add.AddActivity;
 import id.my.avmmartin.matched.utils.CustomCalendar;
 
 public class Activity extends BaseActivity<Presenter> implements MVPView {
     CustomCalendar cvCalendar;
+    ImageButton ibAddSchedule;
+    RecyclerView rvListSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +27,25 @@ public class Activity extends BaseActivity<Presenter> implements MVPView {
     @Override
     protected void initComponents() {
         cvCalendar = findViewById(R.id.cvCalendar);
+        ibAddSchedule = findViewById(R.id.ibAddSchedule);
+        rvListSchedule = findViewById(R.id.rvListSchedule);
     }
 
     @Override
     protected void loadData() {
-        
+
     }
 
     @Override
     protected void setEvents() {
-
+        ibAddSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity.this, AddActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
