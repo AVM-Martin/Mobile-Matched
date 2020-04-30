9f9e4134020947c63b78727ed3887bd41ce27609
AVM.Martin <avmm.martin+github@gmail.com>
2020-04-30 11:18:31 +0700

    Add helper for date time format

--- app/src/main/java/id/my/avmmartin/matched/utils/CommonUtils.java
+++ app/src/main/java/id/my/avmmartin/matched/utils/CommonUtils.java
@@ -3,6 +3,10 @@ package id.my.avmmartin.matched.utils;
 import android.app.ProgressDialog;
 import android.content.Context;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class CommonUtils {
     public static ProgressDialog showLoadingDialog(Context context) {
         ProgressDialog progressDialog = new ProgressDialog(context);
@@ -13,6 +17,16 @@ public class CommonUtils {
         progressDialog.setCanceledOnTouchOutside(false);
 
         return progressDialog;
+    }
+
+    public static String toDateFormat(Calendar calendar) {
+        SimpleDateFormat sdf = new SimpleDateFormat(Constants.DATE_FORMAT, Locale.US);
+        return sdf.format(calendar.getTime());
+    }
+
+    public static String toTimeFormat(Calendar calendar) {
+        SimpleDateFormat sdf = new SimpleDateFormat(Constants.TIME_FORMAT, Locale.US);
+        return sdf.format(calendar.getTime());
     }
 
     private CommonUtils() {
