52c9f8ebc65478d7ad71cddcdf067fd165ca6ad0
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-05 10:29:51 +0700

    Refactor add schedule activity into MVP layers

--- app/src/main/java/id/my/avmmartin/matched/ui/schedule/add/MVPPresenter.java
+++ app/src/main/java/id/my/avmmartin/matched/ui/schedule/add/MVPPresenter.java
@@ -1,7 +1,12 @@
 package id.my.avmmartin.matched.ui.schedule.add;
 
+import android.widget.TextView;
+
+import java.util.Calendar;
+
 import id.my.avmmartin.matched.ui.base.BaseMVPPresenter;
 
-interface MVPPresenter extends BaseMVPPresenter<MVPView> {
-
+public interface MVPPresenter extends BaseMVPPresenter<MVPView> {
+    void showDatePickerDialog(final Calendar calendar, final TextView textView);
+    void showTimePickerDialog(final Calendar calendar, final TextView textView);
 }
--- app/src/main/java/id/my/avmmartin/matched/ui/schedule/add/MVPView.java
+++ app/src/main/java/id/my/avmmartin/matched/ui/schedule/add/MVPView.java
@@ -2,6 +2,7 @@ package id.my.avmmartin.matched.ui.schedule.add;
 
 import id.my.avmmartin.matched.ui.base.BaseMVPView;
 
-interface MVPView extends BaseMVPView {
-
+public interface MVPView extends BaseMVPView {
+    void addSchedule();
+    void cancelSchedule();
 }
