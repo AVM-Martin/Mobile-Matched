1fe070956c70627d25a660e904bd4e292a550833
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-08 01:39:38 +0700

    Implement getPermissionApprovalById method

--- app/src/main/java/id/my/avmmartin/matched/factory/PermissionApprovalFactory.java
+++ app/src/main/java/id/my/avmmartin/matched/factory/PermissionApprovalFactory.java
@@ -8,8 +8,13 @@ import id.my.avmmartin.matched.data.network.firestore.model.PermissionApproval;
 import id.my.avmmartin.matched.exception.ApprovalStatusSetException;
 
 public class PermissionApprovalFactory {
+    private List<PermissionApproval> allApproval = new Vector<>();
     private List<PermissionApproval> pendingApproval = new Vector<>();
     private List<PermissionApproval> approvedApproval = new Vector<>();
+
+    public static List<PermissionApproval> getAllApproval() {
+        return instance.allApproval;
+    }
 
     public static List<PermissionApproval> getPendingApproval() {
         return instance.pendingApproval;
@@ -27,9 +32,11 @@ public class PermissionApprovalFactory {
 
         date.set(2020, 5, 15);
         pendingApproval.add(new PermissionApproval((Calendar) date.clone(), "avm_martin", "ekeitaro"));
+        allApproval.add(new PermissionApproval((Calendar) date.clone(), "avm_martin", "ekeitaro"));
 
         date.set(2020,5, 10);
         pendingApproval.add(new PermissionApproval((Calendar) date.clone(), "ekeitaro", "avm_martin"));
+        allApproval.add(new PermissionApproval((Calendar) date.clone(), "ekeitaro", "avm_martin"));
     }
 
     private void createApprovedApproval() {
@@ -42,11 +49,13 @@ public class PermissionApprovalFactory {
             PermissionApproval a = new PermissionApproval((Calendar) date.clone(), "avm_martin", "ekeitaro");
             a.approve();
             approvedApproval.add(a);
+            allApproval.add(a);
 
             date.set(2020,5, 3);
             PermissionApproval b = new PermissionApproval((Calendar) date.clone(), "ekeitaro", "avm_martin");
             b.approve();
             approvedApproval.add(b);
+            allApproval.add(b);
 
         } catch (ApprovalStatusSetException e) {
             //
