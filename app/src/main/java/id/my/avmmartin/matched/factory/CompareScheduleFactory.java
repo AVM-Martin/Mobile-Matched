99a69707adb398899b3be05673740d30dd0e9ae5
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-08 01:40:37 +0700

    Use singleton style factory

--- app/src/main/java/id/my/avmmartin/matched/factory/CompareScheduleFactory.java
+++ app/src/main/java/id/my/avmmartin/matched/factory/CompareScheduleFactory.java
@@ -9,75 +9,85 @@ import id.my.avmmartin.matched.exception.InvalidDurationException;
 import id.my.avmmartin.matched.exception.NoTitleException;
 
 public class CompareScheduleFactory {
-    public static List<Schedule> getEventLeft() {
-        List<Schedule> result = new Vector<>();
+    private List<Schedule> eventLeft = new Vector<>();
+    private List<Schedule> eventRight = new Vector<>();
 
+    public static List<Schedule> getEventLeft() {
+        return instance.eventLeft;
+    }
+
+    public static List<Schedule> getEventRight() {
+        return instance.eventRight;
+    }
+
+    // creator
+
+    private void createEventLeft() {
         try {
             // first event
             Calendar startA = Calendar.getInstance();
             Calendar endA = Calendar.getInstance();
             startA.set(2020, 5, 7, 7, 0, 0);
             endA.set(2020, 5, 7, 9, 0, 0);
-            result.add(new Schedule("Title", "location", startA, endA));
+            eventLeft.add(new Schedule("Title", "location", startA, endA));
 
             // second event
             Calendar startB = Calendar.getInstance();
             Calendar endB = Calendar.getInstance();
             startB.set(2020, 5, 7, 13, 0, 0);
             endB.set(2020, 5, 7, 14, 0, 0);
-            result.add(new Schedule("Title", "location", startB, endB));
+            eventLeft.add(new Schedule("Title", "location", startB, endB));
 
             // third event
             Calendar startC = Calendar.getInstance();
             Calendar endC = Calendar.getInstance();
             startC.set(2020, 5, 7, 19, 0, 0);
             endC.set(2020, 5, 7, 21, 0, 0);
-            result.add(new Schedule("Title", "location", startC, endC));
+            eventLeft.add(new Schedule("Title", "location", startC, endC));
 
         } catch (NoTitleException e) {
             //
         } catch (InvalidDurationException e) {
             //
         }
-
-        return result;
     }
 
-    public static List<Schedule> getEventRight() {
-        List<Schedule> result = new Vector<>();
-
+    private void createEventRight() {
         try {
             // first event
             Calendar startA = Calendar.getInstance();
             Calendar endA = Calendar.getInstance();
             startA.set(2020, 5, 7, 8, 0, 0);
             endA.set(2020, 5, 7, 10, 0, 0);
-            result.add(new Schedule("Title", "location", startA, endA));
+            eventRight.add(new Schedule("Title", "location", startA, endA));
 
             // second event
             Calendar startB = Calendar.getInstance();
             Calendar endB = Calendar.getInstance();
             startB.set(2020, 5, 7, 12, 0, 0);
             endB.set(2020, 5, 7, 14, 0, 0);
-            result.add(new Schedule("Title", "location", startB, endB));
+            eventRight.add(new Schedule("Title", "location", startB, endB));
 
             // third event
             Calendar startC = Calendar.getInstance();
             Calendar endC = Calendar.getInstance();
             startC.set(2020, 5, 7, 17, 0, 0);
             endC.set(2020, 5, 7, 18, 0, 0);
-            result.add(new Schedule("Title", "location", startC, endC));
+            eventRight.add(new Schedule("Title", "location", startC, endC));
 
         } catch (NoTitleException e) {
             //
         } catch (InvalidDurationException e) {
             //
         }
-
-        return result;
     }
 
+    // singleton
+
+    private static CompareScheduleFactory instance = new CompareScheduleFactory();
+
     private CompareScheduleFactory() {
-        // none
+        createEventLeft();
+        createEventRight();
     }
 }
