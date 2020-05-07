b2d6532980382daf6f04cf0e5330a35730e06a21
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-08 00:33:12 +0700

    Use singleton style factory

--- app/src/main/java/id/my/avmmartin/matched/factory/PermissionApprovalFactory.java
+++ app/src/main/java/id/my/avmmartin/matched/factory/PermissionApprovalFactory.java
@@ -8,21 +8,32 @@ import id.my.avmmartin.matched.data.network.firestore.model.PermissionApproval;
 import id.my.avmmartin.matched.exception.ApprovalStatusSetException;
 
 public class PermissionApprovalFactory {
+    private List<PermissionApproval> pendingApproval = new Vector<>();
+    private List<PermissionApproval> approvedApproval = new Vector<>();
+
     public static List<PermissionApproval> getPendingApproval() {
-        List<PermissionApproval> result = new Vector<>();
-        Calendar date = Calendar.getInstance();
-
-        date.set(2020, 5, 15);
-        result.add(new PermissionApproval((Calendar) date.clone(), "avm_martin", "ekeitaro"));
-
-        date.set(2020,5, 10);
-        result.add(new PermissionApproval((Calendar) date.clone(), "ekeitaro", "avm_martin"));
-
-        return result;
+        return instance.pendingApproval;
     }
 
     public static List<PermissionApproval> getApprovedApproval() {
-        List<PermissionApproval> result = new Vector<>();
+        return instance.approvedApproval;
+    }
+
+    // creator
+
+    private void createPendingApproval() {
+        pendingApproval = new Vector<>();
+        Calendar date = Calendar.getInstance();
+
+        date.set(2020, 5, 15);
+        pendingApproval.add(new PermissionApproval((Calendar) date.clone(), "avm_martin", "ekeitaro"));
+
+        date.set(2020,5, 10);
+        pendingApproval.add(new PermissionApproval((Calendar) date.clone(), "ekeitaro", "avm_martin"));
+    }
+
+    private void createApprovedApproval() {
+        approvedApproval = new Vector<>();
 
         try {
             Calendar date = Calendar.getInstance();
@@ -30,21 +41,24 @@ public class PermissionApprovalFactory {
             date.set(2020, 5, 6);
             PermissionApproval a = new PermissionApproval((Calendar) date.clone(), "avm_martin", "ekeitaro");
             a.approve();
-            result.add(a);
+            approvedApproval.add(a);
 
             date.set(2020,5, 3);
             PermissionApproval b = new PermissionApproval((Calendar) date.clone(), "ekeitaro", "avm_martin");
             b.approve();
-            result.add(b);
+            approvedApproval.add(b);
 
         } catch (ApprovalStatusSetException e) {
             //
         }
-
-        return result;
     }
 
+    // singleton
+
+    private static PermissionApprovalFactory instance = new PermissionApprovalFactory();
+
     private PermissionApprovalFactory() {
-        // none
+        createPendingApproval();
+        createApprovedApproval();
     }
 }
