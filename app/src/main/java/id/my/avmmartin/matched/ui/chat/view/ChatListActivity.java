1f1e986b5f3c0dacaec49c0008feea0db64de8ad
AVM.Martin <avmm.martin+github@gmail.com>
2020-05-10 14:08:08 +0700

    Change intent to ui.chat.details

--- app/src/main/java/id/my/avmmartin/matched/ui/chat/view/ChatListActivity.java
+++ app/src/main/java/id/my/avmmartin/matched/ui/chat/view/ChatListActivity.java
@@ -9,8 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import id.my.avmmartin.matched.R;
 import id.my.avmmartin.matched.ui.base.BaseActivity;
+import id.my.avmmartin.matched.ui.chat.details.ChatDetailsActivity;
 import id.my.avmmartin.matched.ui.chat.view.list.Adapter;
-import id.my.avmmartin.matched.ui.schedule.free.FreeScheduleActivity;
 import id.my.avmmartin.matched.utils.Constants;
 
 public class ChatListActivity extends BaseActivity<Presenter> implements MVPView {
@@ -21,10 +21,9 @@ public class ChatListActivity extends BaseActivity<Presenter> implements MVPView
     // mvp method
 
     @Override
-    public void onApprovedItemClick(String id) {
-        Intent intent = new Intent(this, FreeScheduleActivity.class);
+    public void onChatClick(String id) {
+        Intent intent = new Intent(this, ChatDetailsActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-        intent.putExtra(Constants.INTENT_SELECTED_APPROVAL_ID, id);
         startActivity(intent);
     }
 
@@ -68,7 +67,7 @@ public class ChatListActivity extends BaseActivity<Presenter> implements MVPView
         adapter.setListener(new Adapter.Listener() {
             @Override
             public void onClick(String id) {
-                onApprovedItemClick(id);
+                onChatClick(id);
             }
         });
     }
--- app/src/main/java/id/my/avmmartin/matched/ui/chat/view/MVPView.java
+++ app/src/main/java/id/my/avmmartin/matched/ui/chat/view/MVPView.java
@@ -3,5 +3,5 @@ package id.my.avmmartin.matched.ui.chat.view;
 import id.my.avmmartin.matched.ui.base.BaseMVPView;
 
 public interface MVPView extends BaseMVPView {
-    void onApprovedItemClick(String id);
+    void onChatClick(String id);
 }
